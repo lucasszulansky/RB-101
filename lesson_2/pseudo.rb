a765b4900751ecd5bb830e9b00dded18472128b9
lucasszulansky <lucas.szulansky@gmail.com>
2023-07-07 11:38:14 -0700

    July 7th study

---
+++ lesson_2/pseudo.rb
@@ -0,0 +1,9 @@
+#A method that determines the index of the 3rd occurrence of a given character in a string.
+
+# Set up a match counter variable
+# Set up an index counter variable
+# Define a method that will take two variables = a string and the given character we are looking for
+# Iterate through each index of that string. For each iteration, add one to the index counter for each iteration
+# If the given character matches with that index of the string, add 1 to the counter variable
+# When the counter variable indicates 3 matches have occured, print the index variable
+# If the counter variable doesn't reach 3, return nil
