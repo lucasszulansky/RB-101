029fe8eddd5097b08d0d9e601e8ed4672fe6f0d7
lucasszulansky <lucas.szulansky@gmail.com>
2023-07-12 12:17:46 -0700

    Finished Easy RB101 problems

---
+++ lesson_3/scratch.rb
@@ -0,0 +1,13 @@
+def factors(number)
+  divisor = number
+  factors = []
+  begin
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1
+  end until divisor == 0
+  factors
+end
+
+puts "Enter number"
+answer = gets.chomp.to_i
+puts factors(answer)
